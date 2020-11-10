75eed69502429e57759759c962a29415228417ee
ritakarmen <ritakarmen@gmail.com>
2020-11-10 07:10:29 -0500

    mejoras mobil y descktop

--- js/main.js
+++ js/main.js
@@ -27,45 +27,4 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 /***---*** */
-/* slider list home*/
 
-let i = 1;
-for(let li of carousel.querySelectorAll('li')) {
-  li.style.position = 'relative';
-  li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
-  i++;
-}
-
-
-let width = 130; 
-let count = 3; 
-
-let list = carousel.querySelector('ul');
-let listElems = carousel.querySelectorAll('li');
-
-let position = 0; 
-
-carousel.querySelector('.prev').onclick = function() {
-
-  position += width * count;
-  
-  position = Math.min(position, 0)
-  list.style.marginLeft = position + 'px';
-};
-
-carousel.querySelector('.next').onclick = setInterval(function() {
-
-  position -= width * count;
- 
-  position = Math.max(position, -width * (listElems.length - count));
-  list.style.marginLeft = position + 'px';
-}, 5000);
-
-/* SIDER NARANJ */
-
-var comtem = document.getElementsByClassName("carousel__slide");
-var n = 9;
-
-  console.log('fff',comtemtt)
-
-/** */
